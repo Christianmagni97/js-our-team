f16e8725228b825cae4e632f47721a3ffb5f98ae
Christianmagni97 <christianmagni28@gmail.com>
2024-05-23 00:18:06 +0200

    prove con js su objets

---
+++ script.js
@@ -0,0 +1,44 @@
+const workers [
+    {
+        name = 'Wayne Barnet',
+        work = 'Founder e CEO'
+    }
+    {
+        name:  'Angela Caroll' ,
+        work: 'Chief Editor' ,
+
+    }
+    {
+        name:  'Angela Caroll' ,
+        work: 'Chief Editor' ,
+    }
+    {
+        name:  'Angela Caroll' ,
+        work: 'Chief Editor' ,
+    }
+    {
+        name:  'Walter Gordon' ,
+        work: 'Office Manager' ,
+    }
+    {
+        name:  'Walter Gordon' ,
+        work: 'Office Manager' ,
+    }
+    {
+        name:  'Angela Lopez' ,
+        work: 'Social media Manager' ,
+    }
+    {
+        name:  'Angela Lopez' ,
+        work: 'Social media Manager' ,
+    }
+    {
+        name:  'Scott Estrada' ,
+        work: 'Developer' ,
+    }
+    {
+        name:  'Barbara Ramos' ,
+        work: 'Graphic designer' ,
+    }
+]
+console.log(workers);
